0287f3ad6a37a6a81699e2921a894e39c4774ee9
Peter Osoo <posoo617@gmail.com>
2023-03-10 20:01:53 +0300

    Person.jsx add notes and tailwind styles

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,19 +1,27 @@
-import avatar from '../assets/default-avatar.png';
+import avatar from "../assets/default-avatar.png"
 
+const Person = ({
+	name,
+	nickName = "WuodLuo",
+	images,
+	notes = "Self taught dev",
+}) => {
+	// before optional chaining
 
-const Person = ({ name, nickName = 'WuodLuo', images }) => {
-  // before optional chaining
+	// const img =
+	//   (images && images[0] && images[0].small && images[0].small.url) || avatar;
 
-  // const img =
-  //   (images && images[0] && images[0].small && images[0].small.url) || avatar;
-  
-  const img = images?.[0]?.small?.url || avatar;
-  return (
-    <div>
-      <img src={img} alt={name} style={{ width: '50px' }} />
-      <h4>{name} </h4>
-      <p>Nickname : {nickName}</p>
-    </div>
-  );
-};
-export default Person;
+	const img = images?.[0]?.small?.url || avatar
+	return (
+		<section class="text-gray-600 body-font px-20 pt-10">
+			<img src={img} alt={name} style={{ width: "50px" }} />
+
+			<div class="w-full">
+				<h2 class="title-font font-medium text-lg text-gray-900">{name}</h2>
+				<h3 class="text-gray-500 mb-3"> Nickname : {nickName} </h3>
+				<p class="mb-4">Notes: {notes}</p>
+			</div>
+		</section>
+	)
+}
+export default Person
