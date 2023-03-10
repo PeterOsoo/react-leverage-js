fccf6a58e465dcddd4ac2a017c397513e1eff197
Peter Osoo <posoo617@gmail.com>
2023-03-10 19:48:24 +0300

    Person component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import List from "./components/List"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <List />
+    </div>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const people = [
+	{ id: 1, name: "jaka", nickName: "Stud Muffin" },
+	{ id: 2, name: "peter" },
+	{
+		id: 3,
+		name: "ondiek",
+		images: [
+			{
+				small: {
+					url: "https://res.cloudinary.com/diqqf3eq2/image/upload/ar_1:1,bo_5px_solid_rgb:ff0000,c_fill,g_auto,r_max,w_1000/v1595959121/person-1_aufeoq.jpg",
+				},
+			},
+		],
+	},
+	{ id: 4, name: "wuodluo" },
+]
